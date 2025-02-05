0408c59c4b738e5bdf1999ed189d8c1fb0207ac1
IoTSabaAzizzadeh <iot.sabaazizzadeh@gmail.com>
2025-02-05 15:16:52 +0330

    this is a commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function counter(arr) {
 }
 
 const input = ['a', 'b', 'a', 'c', 'b', 'a'];
-console.log(counter(input)); 
+console.log(counter(input));
 
 
 function transformArray(arr) {
@@ -20,4 +20,42 @@ function transformArray(arr) {
 }
 
 const input2 = ['Saba', 'Sara', 'Ayda'];
-console.log(transformArray(input2)); 
+console.log(transformArray(input2));
+
+
+function cardGenerator() {
+
+    const titleEL = document.createElement("div")
+    const pEL = document.createElement("p")
+    const buttonEL = document.createElement("button")
+    console.log(titleEL, pEL, buttonEL);
+
+    titleEL.classList.add('title')
+    pEL.classList.add('content')
+    buttonEL.classList.add('button')
+
+    titleEL.textContent = 'Title'
+    document.body.append(titleEL)
+
+    pEL.textContent = 'this is paragraph'
+    document.body.append(pEL)
+
+    buttonEL.textContent = 'delete'
+    document.body.append(buttonEL)
+
+    
+        const deleteButton = document.querySelector('.button')
+        deleteButton.addEventListener(('click'), () => {
+            const deleteTitle = document.querySelector('.title')
+            const deleteContent = document.querySelector('.content')
+            const deleteButton = document.querySelector('.button')
+
+            deleteTitle.remove(); // Remove the box element
+            deleteContent.remove(); // Remove the box element
+            deleteButton.remove(); // Remove the box element
+        })
+    
+
+
+}
+
